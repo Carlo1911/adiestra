b8db8f7652f5540f3717fc93003db38ce3c0ab76
Carlo Alva <carlo0071@gmail.com>
2017-11-21 13:02:28 -0500

    examen

--- adiestra/settings.py
+++ adiestra/settings.py
@@ -26,6 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+NUMERO_JUGADAS = 6
 
 # Application definition
 
--- examen/views.py
+++ examen/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from random import randint
+from django.conf import settings
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class Index(TemplateView):
+    template_name = 'jugadas.html'
+
+    def get_context_data(self, **kwargs):
+        jugadas = []
+        for i in range(settings.NUMERO_JUGADAS):
+            jugadas.append(generar_jugada())
+        context = {'jugadas': jugadas}
+        return context
+
+
+def generar_jugada():
+    numeros = []
+    while(len(numeros) < 6):
+        numero = str(randint(0, 45)).zfill(2)
+        if numero not in numeros:
+            numeros.append(numero)
+
+    jugada = '-'.join(sorted(numeros))
+    return jugada
